c2deba20f49b14173b0afaae3d3088038ba3e797
santanam23 <mirmir11.ms@gmail.com>
2022-02-05 13:49:01 -0600

    editing GET/POST/DELETE API request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+const req = require("express/lib/request");
 
 //How to handle asynchronous processes
 const readFileAsync = util.promisify(fs.readFile);
@@ -23,6 +24,28 @@ app.get("/api/notes", function(req, res) {
     readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
+    })
+});
+
+
+// API ROUTE POST REQUEST
+app.post("/api/notes", function(req,res) {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json" , JSON.stringify(notes))
+    })
+});
+
+//API ROUTE DELETE REQUEST 
+app.delete("/api/notes/:id", function(req,res) {
+    const idToDelete = parseInt(req.params.id);
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
     })
 });
 
